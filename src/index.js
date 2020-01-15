1bd4dacaf761262ac8f9cedbc03983a9f586ccbe
Evia-H <evi1020@gmail.com>
2020-01-15 21:26:29 +0200

    added class component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import HomePage from "./components/HomePage";
+import About from "./components/AboutPage";
 import { render } from "react-dom";
 
-render(<HomePage />, document.getElementById("root"));
+render(<About />, document.getElementById("root"));
