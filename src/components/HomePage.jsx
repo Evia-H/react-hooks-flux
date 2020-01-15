f74d3c827f5a6aad875f75b104f73db1be32a150
Evia-H <evi1020@gmail.com>
2020-01-15 20:52:09 +0200

    created first Function Component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Evia's Project</h1>
+      <p>React, Flux, Hooks and React Router</p>
+    </div>
+  );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import HomePage from "./components/HomePage";
+import { render } from "react-dom";
+
+render(<HomePage />, document.getElementById("root"));
