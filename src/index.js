7c0db6d4183152df1bfd8456c8b09c900ae3ffd4
Evia-H <evi1020@gmail.com>
2020-01-18 16:12:45 +0200

    initial Router configuration, routes declartion

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import App from "./components/App";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
