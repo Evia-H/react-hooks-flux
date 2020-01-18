5397f582a2c8fdc915c57e38cbb61a8499a81827
Evia-H <evi1020@gmail.com>
2020-01-18 16:23:51 +0200

    links and navlinks

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
+  const ActiveStyle = { color: "orange" };
   return (
     <nav>
-      <a href="/">Home</a> | <a href="/courses">Courses</a> |{" "}
-      <a href="/about">About</a>
+      <NavLink activeStyle={ActiveStyle} exact to="/">
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink activeStyle={ActiveStyle} to="/courses">
+        Courses
+      </NavLink>
+      {" | "}
+      <NavLink activeStyle={ActiveStyle} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 }
